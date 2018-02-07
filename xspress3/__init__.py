13594984ebb0bf3c1e84a1be6fe85cd645399ba5
Stu Fisher <stu@q3f.org>
2018-02-07 11:20:04 +0100

    add docstrings

--- xspress3/__init__.py
+++ xspress3/__init__.py
@@ -13,11 +13,13 @@ class Xspress3:
     """Xspress 3 Device
 
     A class encapsulating an Xspress 3 Device
+
+    Example:
       >>> x3 = Xspres3(pv_prefix)  # create an x3 object with pv_prefix
       >>> x3.set(
       >>>     exposure_time=1,
       >>>     num_images=10
-      >>> })
+      >>> )
       >>> x3.acquire()
     """
 
@@ -50,6 +52,15 @@ class Xspress3:
 
 
     def __init__(self, prefix, logger=None, subframes=False):
+        """ Create an Xspress 3 device instance
+
+        Args:
+            prefix (string): the PV prefix of the device, eg. XSPRESS3-EXAMPLE
+
+        Kwargs:
+            subframes (bool): enable subframe parmeters (special IOC required)
+
+        """
         self.logger = logger or logging.getLogger(__name__)
 
         self._prefix = prefix
@@ -91,11 +102,30 @@ class Xspress3:
         # Short delay to wait for PV readbacks
         time.sleep(0.2)
 
+
     def channels(self):
+        """ Get the number of channels on the Xspress 3 system
+
+        Returns:
+            no_channels (int): The number of channels on the system
+        """
         return self._channels
 
 
     def add_frame_callback(self, callback):
+        """ Add a frame change callback
+
+        Adds a callback to the list of callbacks called when a frame change occurs
+        during an acquisition. The callback recieves the frame number as an argument
+
+        >>> def callback(frame_number):
+        >>>     print 'New frame {f}'.format(frame_number)
+        >>> x3.add_frame_callback(callback)
+
+        Args:
+            callback (callable): the callback to add to the list of frame callbacks
+
+        """
         assert not callback in self._frame_callbacks, 'Callback already registered'
         self._frame_callbacks.append(callback)
 
@@ -120,22 +150,39 @@ class Xspress3:
 
 
     def dropped_frames(self):
+        """ Return the number of dropped frames in the last acquisition
+
+        At high frames rates EPICS cannot keep up with PVs changing quickly,
+        in these situations use file saving and read the hdf5 with the hdf5 
+        module. Tests shows this occurs above around 100Hz
+
+        This value will tell you how many frames were dropped
+    
+        Returns:
+            dropped_frames (int): the number of dropped frames in the last acquisition
+
+        """
         return self._num_acquired - self._acquired_iter
 
 
     def set(self, **kwargs):
-        """set an attribute on the device
+        """ Set an attribute on the device
 
-        Arguments
-        =========
-        exposure_time    (float) per image exposure time in seconds
-        num_images       (int) number of images to acquire
-        trigger_mode     (string) trigger mode (Internal, External)
+        Kwargs:
+            exposure_time (float): per image exposure time in seconds
 
-        file_path        (string) path to save hdf5 files to
-        file_name        (string) file template for hdf5
-        file_number      (int) file number ({file_path}/{file_name}{file_number}.hdf5)
-        file_capture     (boolean / int) enable hdf5 saving
+            num_images (int): number of images to acquire
+
+            trigger_mode (string): trigger mode (Internal, External)
+
+            file_path (string): path to save hdf5 files to
+
+            file_name (string): file template for hdf5
+
+            file_number (int): file number ({file_path}/{file_name}{file_number}.hdf5)
+            
+            file_capture (bool): enable hdf5 saving
+
         """
 
         for p,v in kwargs.iteritems():
@@ -150,6 +197,7 @@ class Xspress3:
                 assert val is not None, 'Invalid value {val} for parameter {param}. Available values are: {vals}'.format(
                     val=v, param=p, vals=','.join(self._parameters[p][3].values()))
                 caput(self._pv(self._parameters[p][0]), val)
+
             else:
                 caput(self._pv(self._parameters[p][0]), v)
 
@@ -157,11 +205,14 @@ class Xspress3:
 
 
     def get(self, param=None):
-        """get the value of an attribute
+        """ Get the value of an attribute
 
-        Arguments
-        =========
-        param    (string) parameter to get
+        Args:
+            param (string): parameter to return the value of
+
+        Returns:
+            value (mixed): the value of the parameter
+
         """
 
         if param is None:
@@ -180,6 +231,20 @@ class Xspress3:
 
 
     def setup_file_saving(self, dirn, template):
+        """ Setup hdf5 file saving
+
+        >>> x3.setup_file_saving('/home/test/data', 'test')
+        >>> '/home/test/data/test{number}.hdf5'
+
+        Args:
+            dirn (string): directory to save hdf5 files to
+
+            template (string): file template of hdf5 files
+
+        Returns:
+            filename (string): the filename of the last hdf5 captured
+
+        """
         self.set(
             file_path=dirn,
             file_name=template,
@@ -189,17 +254,34 @@ class Xspress3:
 
         return self.filename()
 
+
     def file_saving(self, enable):
+        """ Enable file saving
+
+        File saving is disabled after each acquisition, renable or disable it
+
+        Args:
+            enable (bool): enable or disable file saving
+
+        """
         self.set(file_capture=1 if enable else 0)
 
         return self.filename()
 
 
     def filename(self):
+        """ Returns the current/last hdf5 filename
+
+        Returns:
+            filename (string): the current hdf5 filename
+
+        """
         return self._file_name.value()
 
 
     def acquire(self):
+        """ Starts an acquisition """
+
         self._acquired_iter = 0
         self._num_acquired = 0
 
@@ -215,22 +297,68 @@ class Xspress3:
         self.logger.info('Acquiring')
 
     def stop(self):
+        """ Stops an acquisiton """
+
         self.logger.info('Aborting Acquisition')
         caput(self._pv('Acquire'), 0)
 
     def acquiring(self):
+        """ Returns the acquisition status
+
+        Returns:
+            acquiring (bool): the acquisition status of the device
+
+        """
         return self._acq_status.value()
 
     def num_acquired(self):
+        """ Returns the number of frames acquired
+
+        Returns:
+            num_acquired (int): the number of frames acquired
+
+        """
         return self._num_acquired
 
 
     def mca(self, chan):
+        """ Returns the MCA for the specified channel
+
+        This value will be automatically updated whenever the PV changes
+
+        Args:
+            chan (int): channel number to return MCA for, zero offset
+
+        Returns:
+            mca (list[int]): return a list of mca values (usually 4096 values)
+
+        """
         assert chan < len(self._mcas), 'Chan {chan} > number of channels {chans}'.format(chan=chan, channels=self._channels)
 
         return self._mcas[chan].value()
 
     def sca(self, chan, sca):
+        """ Returns the specified scalar for the specified channel
+
+        Args:
+            chan (int): channel number to return scalar for, zero offset
+
+            sca (int): scalar to return
+
+        Returns:
+            scalar (int): the specified scalar value
+
+        Available scalars:
+            0. Time in ticks
+            1. ResetTicks
+            2. ResetCount
+            3. AllEvent
+            4. AllGood
+            5. InWindow 0
+            6. InWindow 1
+            7. PileUp
+
+        """
         assert chan < len(self._scalars), 'Chan {chan} > number of channels {chans}'.format(chan=chan, channels=self._channels)
         assert sca < len(self._scalars[chan]), 'Scalar {sca} > number of scalars {scalars}'.format(chan=sca, channels=self._channels)
 
--- xspress3/hdf5.py
+++ xspress3/hdf5.py
@@ -6,8 +6,27 @@ import numpy as np
 
 
 class HDF5:
+    """Xspress 3 HDF5 Parser
+
+    A class for parsing Xspress 3 HDF5 files.
+    Is context managed so files are automatically closed when done
+
+    Example:
+      >>> with HDF5(file) as h5:
+      >>>     h5.size()
+      >>>     h5.mca(0,0)
+      >>>
+      >>> {'channels': 1, 'frames': 4, 'bins': 4096}
+      >>> [0,0,0....0]
+    """
 
     def __init__(self, file, logger=None):
+        """ Create an Xspress 3 HDF5 parser instance
+
+        Args:
+            file (string): the hdf5 file to open
+
+        """
         self.logger = logger or logging.getLogger(__name__)
 
         self.logger.info('Loading {file}'.format(file=file))
@@ -32,6 +51,16 @@ class HDF5:
 
 
     def size(self):
+        """ Returns the dimensions of the HDF5 file
+
+        Returns:
+            size (dict): the hdf5 file dimensions
+            
+                * channels (int): number of channels in the file
+                * frames (int): number of frames in the file
+                * bins (int): number of bins per mca in the file
+
+        """
         return {
             'channels': self._channels,
             'frames': self._frames,
@@ -40,27 +69,54 @@ class HDF5:
 
 
     def close(self):
+        """ Manually close the hdf5 file """
+
         self._file.close()
 
 
     def mca(self, chan, frameno):
+        """ Returns the specified MCA
+
+        Args:
+            chan (int): the channel to return an MCA for
+
+            frameno (int): the frame number to return an MCA for
+
+        Returns:
+            mca (list[int]): the MCA as a list (usually with 4096 elements)
+
+        """
         assert chan < self._channels, 'Channel {chan} out of range of channels {chans}'.format(chan=chan, chans=self._channels)
         assert frameno < self._frames, 'Frame no {fr} out of range of frames {frs}'.format(fr=frameno, frs=self._frames)
 
         return self._data[frameno,chan,:].astype(int).tolist()
 
 
-    # Scalars:
-    #   0 = Time in ticks
-    #   1 = ResetTicks
-    #   2 = ResetCount
-    #   3 = AllEvent
-    #   4 = AllGood
-    #   5 = InWindow 0
-    #   6 = InWindow 1
-    #   7 = PileUp
-    #   
     def sca(self, chan, frameno, sca):
+        """ Returns the specified scalar
+
+        Aargs:
+            chan (int): channel number to return scalar for, zero offset
+
+            frameno (int): the frame number to return scalar for, zero offset
+
+            sca (int): scalar to return
+
+        Returns:
+            scalar (int): the specified scalar value
+
+    
+        Available scalars:
+            0. Time in ticks
+            1. ResetTicks
+            2. ResetCount
+            3. AllEvent
+            4. AllGood
+            5. InWindow 0
+            6. InWindow 1
+            7. PileUp
+
+        """
         assert chan < self._channels, 'Channel {chan} out of range of channels {chans}'.format(chan=chan, chans=self._channels)
         assert frameno < self._frames, 'Frame no {fr} out of range of frames {frs}'.format(fr=frameno, frs=self._frames)
 
@@ -72,18 +128,31 @@ class HDF5:
 
 
     def dtc(self, chan, frameno):
+        """ Returns the specified deadtime correction parameters
+
+        Args:
+            chan (int): channel number to return scalar for, zero offset
+
+            frameno (int): the frame number to return scalar for, zero offset
+
+        Returns:
+            dtc_params (list): returns a list of two dead time correction params:
+                0. dead time correction factor
+                1. dead time percentage
+
+        """
         assert chan < self._channels, 'Channel {chan} out of range of channels {chans}'.format(chan=chan, chans=self._channels)
         assert frameno < self._frames, 'Frame no {fr} out of range of frames {frs}'.format(fr=frameno, frs=self._frames)
 
         dtfid = 'CHAN{chan}DTFACTOR'.format(chan=(chan+1))
         dtf = np.array(self._attrs.get(dtfid))
 
-        assert dtf is not None, 'No such attribute {attr}'.format(attr=attrid)
+        assert dtf is not None, 'No such attribute {attr}'.format(attr=dtfid)
 
         dtpid = 'CHAN{chan}DTPERCENT'.format(chan=(chan+1))
         dtp = np.array(self._attrs.get(dtpid))
 
-        assert dtf is not None, 'No such attribute {attr}'.format(attr=attrid)
+        assert dtf is not None, 'No such attribute {attr}'.format(attr=dtpid)
 
         return [dtf[frameno], dtp[frameno]]
 
